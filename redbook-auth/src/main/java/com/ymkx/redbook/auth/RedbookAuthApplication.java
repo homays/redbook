2d94d9428567c4fb6f09117a7eb73cdff7f13d7b
huangrui <2362552845@qq.com>
2025-08-06 17:34:58 +0800

    feat: 自定义 Spring Boot 3.x Starter: 封装 API 请求日志切面业务组件

--- redbook-auth/src/main/java/com/ymkx/redbook/auth/RedbookAuthApplication.java
+++ redbook-auth/src/main/java/com/ymkx/redbook/auth/RedbookAuthApplication.java
@@ -8,6 +8,7 @@ public class RedbookAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedbookAuthApplication.class, args);
+        System.out.println("redbook-auth ===> start success");
     }
 
 }
