2d9aef96e060073d8f925d2eea3de7cade73459f
huangrui <2362552845@qq.com>
2025-08-06 17:10:08 +0800

    feat: 初始架构

---
+++ redbook-auth/src/main/java/com/ymkx/redbook/auth/RedbookAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ymkx.redbook.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbookAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedbookAuthApplication.class, args);
+    }
+
+}
---
+++ redbook-auth/src/test/java/com/ymkx/redbook/auth/RedbookAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymkx.redbook.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbookAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
