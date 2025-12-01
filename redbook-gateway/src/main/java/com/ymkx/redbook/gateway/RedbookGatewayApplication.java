b3879fa7a45c80128e6fc92d6137d451a13c91f5
huangrui <rui.huangr@dtzhejiang.com>
2025-12-01 17:44:53 +0800

    feat: 新增网关服务

---
+++ redbook-gateway/src/main/java/com/ymkx/redbook/gateway/RedbookGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ymkx.redbook.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbookGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedbookGatewayApplication.class, args);
+    }
+
+}
